2a16318ead02b47dda61ac88d5e964dfb04be2e4
Micah Redding <micahtredding@gmail.com>
2013-08-03 19:29:42 -0500

    better Facebook integration

--- sites/all/themes/micah/templates/page.tpl.php
+++ sites/all/themes/micah/templates/page.tpl.php
@@ -147,12 +147,3 @@
       </div>
     </div>
   </footer>
-
-  <div id="fb-root"></div>
-  <script>(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=275866199116127";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));</script>
