0cd4adc8c0dafeda239e251d5fa701d5d3f786d7
Micah Redding <micahtredding@gmail.com>
2015-01-17 22:13:56 -0600

    Add image style attributes pre-processor

--- sites/all/themes/bartik/template.php
+++ sites/all/themes/bartik/template.php
@@ -155,3 +155,28 @@ function bartik_field__taxonomy_term_reference($variables) {
 
   return $output;
 }
+
+
+function bartik_image_style($variables) {
+  // $style_name = $variables['style_name'];
+  // $path = $variables['path'];
+
+  // // theme_image() can only honor the $getsize parameter with local file paths.
+  // // The derivative image is not created until it has been requested so the file
+  // // may not yet exist, in this case we just fallback to the URL.
+  // $style_path = image_style_path($style_name, $path);
+  // if (!file_exists($style_path)) {
+  //   $style_path = image_style_url($style_name, $path);
+  // }
+  // $variables['path'] = $style_path;
+
+  // if (is_file($style_path)) {
+  //   if (list($width, $height, $type, $attributes) = @getimagesize($style_path)) {
+  //     $variables['width'] = $width;
+  //     $variables['height'] = $height;
+  //   }
+  // }
+
+  $variables['attributes'] = array("style" => "max-width: 100%; height:auto;");
+  return theme('image', $variables);
+}
